84c1815775ea7e8beee6610433bc5937b245fb8e
j-trueman <josh@joshtrueman.co.uk>
2024-04-15 02:02:12 +0100

    basic user creation and authentication

--- mods-unpacked/GlitchedData-MultiplayerMod/utils/MultiplayerManager.gd
+++ mods-unpacked/GlitchedData-MultiplayerMod/utils/MultiplayerManager.gd
@@ -9,46 +9,76 @@ var my_info = {"Name": "GlitchedData64"}
 var players_loaded
 var current_lobby
 var is_host
-var keygen_util
+var keygenUtil
+var accountName = "jeremy"
+var userSignature
+var keyFile = CryptoKey.new()
+var userHasCorrectKey
+var sessionEnded
+var loggedIn
 
 func _ready():
-	multiplayer.peer_connected.connect(_on_player_connected)
+#	multiplayer.peer_connected.connect(_on_player_connected)
 	multiplayer.peer_disconnected.connect(_on_player_disconnected)
 	multiplayer.connected_to_server.connect(_on_connected_ok)
 	multiplayer.connection_failed.connect(_on_connected_fail)
 	multiplayer.server_disconnected.connect(_on_server_disconnected)
+	
+	keygenUtil = get_tree().root.get_node("KeygenUtil")
+	connectToServer()
+	await multiplayer.connected_to_server
+	print("SUCCESSFULLY CONNECTED TO SERVER. ATTEMPTING LOGIN")	
+	doLoginStuff()
 
-func _input(ev):
-	if Input.is_key_pressed(KEY_J):
-		join_game()
-		keygen_util = get_tree().root.get_node("KeygenUtil")
-	if Input.is_key_pressed(KEY_C):
-		create_lobby.rpc(2, "round robin", "public")
-		is_host = true
-	if Input.is_key_pressed(KEY_V):
-		create_lobby.rpc(2, "round robin", "private")
-		is_host = true
-	if Input.is_key_pressed(KEY_L):
-		request_lobby_list.rpc()
-	if Input.is_key_pressed(KEY_K):
-		var signature = keygen_util.KeyGen()
-		create_new_multiplayer_user.rpc("GlitchedData", signature)
-
-func join_game(address = ""):
-	if address.is_empty():
-		address = "localhost"
+func connectToServer():
 	var peer = ENetMultiplayerPeer.new()
-	var error = peer.create_client(address, 2244)
+	var error = peer.create_client("localhost", 2244)
 	if error:
+		print("ERROR: %s" % error)
 		return error
+#	await multiplayer.connected_to_server
 	multiplayer.multiplayer_peer = peer
-	print("JOINED GAME")
+
+@rpc("authority")
+func closeSession(reason):
+	multiplayer.multiplayer_peer = null
+	sessionEnded = true
+	print("MULTIPLAYER SESSION TERMINATED: '%s'" % reason)
+
+func doLoginStuff():
+	var userKeyExists = checkForUserKey()
+	if !userKeyExists:
+		print("NO USER KEY DETECTED. PLEASE CREATE AN ACCOUNT IN THE CONFIG MENU.")
+		closeSession("No user key detected")
+		return false
+	verifyUserCreds.rpc(accountName, keyFile.save_to_string())
+
+func checkForUserKey():
+	var error = keyFile.load("user://privatekey.key")
+	if error:
+		return false
+	return true
+
+@rpc("authority")
+func notifySuccessfulLogin():
+	print("SUCCESSFULLY LOGGED IN AS %s" % accountName)
+	loggedIn = true
+
+#func JoinGame(address = ""):
+#	if address.is_empty():
+#		address = "localhost"
+#	var peer = ENetMultiplayerPeer.new()
+#	var error = peer.create_client(address, 2244)
+#	if error:
+#		return error
+#	multiplayer.multiplayer_peer = peer
+#	print("INITIAL CONNECTION")
 
 func remove_multiplayer_peer():
 	multiplayer.multiplayer_peer = null
 
-func _on_player_connected(id):
-	_register_player.rpc_id(id, my_info)
+#func _on_player_connected(id):
+#	_register_player.rpc_id(id, my_info)
 
 @rpc("any_peer", "reliable")
 func _register_player(new_player_info):
@@ -94,8 +124,30 @@ func user_creation_status(return_value: bool):
 	else:
 		print("CREATED USER SUCCESSFULLY")
 
+
 # GHOST FUNCTIONS
 @rpc("any_peer") func close_lobby(): pass
 @rpc("any_peer", "reliable") func create_lobby(): pass
 @rpc("any_peer") func request_lobby_list(): pass
 @rpc("any_peer", "reliable") func create_new_multiplayer_user(username: String, signature : PackedByteArray) : pass
+@rpc("any_peer") func verifyUserCreds(username: String, keyFile : Resource): pass
+
+# DEBUG INPUTS
+func _input(ev):
+	if Input.is_key_pressed(KEY_C):
+		create_lobby.rpc(2, "round robin", "public")
+		is_host = true
+	if Input.is_key_pressed(KEY_V):
+		create_lobby.rpc(2, "round robin", "private")
+		is_host = true
+	if Input.is_key_pressed(KEY_L):
+		request_lobby_list.rpc()
+	if Input.is_key_pressed(KEY_K):
+		var signature = keygenUtil.KeyGen()
+		if !signature:
+			print("YOU ALREADY HAVE AN ACCOUNT")
+			return false
+		if !loggedIn:
+			connectToServer()
+			await multiplayer.connected_to_server
+			create_new_multiplayer_user.rpc(accountName, signature)
--- mods-unpacked/GlitchedData-MultiplayerMod/utils/keygenutil.gd
+++ mods-unpacked/GlitchedData-MultiplayerMod/utils/keygenutil.gd
@@ -2,6 +2,7 @@ extends Node
 
 var crypto = Crypto.new()
 var privateKey = CryptoKey.new()
+var signatureData = "multiplayersignature"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,9 +11,18 @@ func _ready():
 func _process(delta):
 	pass
 
-func KeyGen() -> PackedByteArray:
+func KeyGen():
+	privateKey.load("user://privatekey.key")
+	if privateKey.save_to_string() != "":
+		return false
 	privateKey = crypto.generate_rsa(4096)
-	privateKey.save("user://privatekey.pub")
-	var data = "multiplayersignature"
-	var signature = crypto.sign(HashingContext.HASH_SHA256, data.sha256_buffer(), privateKey)
+	privateKey.save("user://privatekey.key")
+	var signature = crypto.sign(HashingContext.HASH_SHA256, signatureData.sha256_buffer(), privateKey)
+	privateKey = CryptoKey.new()
 	return signature
+
+func verifyUserSignature(signature, key):
+	var signatureMatches = crypto.verify(HashingContext.HASH_SHA256, signatureData.sha256_buffer(), signature, key)
+	if !signatureMatches:
+		return false
+	return true
